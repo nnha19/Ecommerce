c6fd3b5dce70cdddf18271553b54a30fe5977072
WebTek828 <webtekprogrammer@gmail.com>
2021-02-16 19:27:37 +0630

    customer routes with jwt and bcrypt finishes in backend

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,4 +1,6 @@
 const Customer = require("../Modals/Customer");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const createCustomer = async (req, res, next) => {
   try {
@@ -7,12 +9,46 @@ const createCustomer = async (req, res, next) => {
     if (existCustomer) {
       res.status(400).json("User with this email already exists.");
     } else {
+      const hashedPassword = await bcrypt.hash(password, 12);
       const newCustomer = await Customer.create({
         username,
         email,
-        password,
+        password: hashedPassword,
       });
-      res.status(200).json("Account successfully created.");
+      const token = await jwt.sign(
+        { userId: newCustomer._id, username },
+        "Secret_Key",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ message: "Account successfully created", token });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+const loginCustomer = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const customer = await Customer.findOne({ email });
+    if (!customer) {
+      res.status(400).json("User with the provided email doesn't exist");
+    } else {
+      const validPassword = await bcrypt.compare(password, customer.password);
+      if (validPassword) {
+        const token = jwt.sign(
+          {
+            userId: customer._id,
+            username: customer.username,
+          },
+          "Secret_Key",
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ message: "Logged you in", token });
+      } else {
+        res.status(400).json("Incorrect password.");
+      }
     }
   } catch (err) {
     console.log(err);
@@ -21,3 +57,4 @@ const createCustomer = async (req, res, next) => {
 };
 
 exports.createCustomer = createCustomer;
+exports.loginCustomer = loginCustomer;
--- server/routes/customerRoute.js
+++ server/routes/customerRoute.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const customerController = require("../controllers/customerController");
 
 router.post("/", customerController.createCustomer);
+router.post("/login", customerController.loginCustomer);
 
 module.exports = router;
