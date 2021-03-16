57eb42b71a68ff54d418af7044f9a8f3cebbfc48
Nyi Nyi Hmue Aung <webtekprogrammer@gmail.com>
2021-03-16 16:21:12 +0630

    update changes

--- client/src/admin/components/AllCoupons/AllCoupons.js
+++ client/src/admin/components/AllCoupons/AllCoupons.js
@@ -1,10 +1,36 @@
 import React from "react";
 
+import "./AllCoupons.css";
+
 const AllCoupons = (props) => {
+  let couponOutput;
+
+  if (props.coupons.length === 0) {
+    couponOutput = (
+      <div>
+        <p>No Coupons</p>;
+      </div>
+    );
+  } else {
+    couponOutput = props.coupons.map((coupon) => {
+      return (
+        <div key={coupon._id} className="coupon-output">
+          <span>{coupon.code}</span>
+          <span>{coupon.discountPrice}</span>
+          <div className="coupon-mani"></div>
+        </div>
+      );
+    });
+  }
+
   return (
-    <div>
-      <p>All Coupons</p>
-    </div>
+    <>
+      <div className="coupon-wrapper">
+        <h4>All Coupons</h4>
+
+        <div className="coupons">{couponOutput}</div>
+      </div>
+    </>
   );
 };
 
--- client/src/admin/pages/AllCouponsPage.js
+++ client/src/admin/pages/AllCouponsPage.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 
 import AllCoupons from "../components/AllCoupons/AllCoupons";
+import { useHttp } from "../../customHooks/useHttp";
+import Context from "../../contexts/context";
 
 const AllCouponsPage = (props) => {
+  const context = useContext(Context);
+
+  const [
+    allCoupons,
+    loading,
+    error,
+    fetchData,
+    setAllCoupons,
+    setError,
+  ] = useHttp("");
+
+  useEffect(() => {
+    fetchData(
+      `${process.env.REACT_APP_BACKEND_URL}/coupon`,
+      "get",
+      "",
+      context.token
+    );
+  }, []);
+
   return (
     <>
-      <AllCoupons />
+      <AllCoupons coupons={allCoupons} />
     </>
   );
 };
