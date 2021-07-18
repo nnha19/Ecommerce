99617c23ee42fb3434ca5582c3da439d77e3f93a
webtek <webtekprogrammer@gmail.com>
2021-07-18 13:02:06 +0630

    all product component design fixes

--- client/src/products/components/AllProducts/AllProducts.js
+++ client/src/products/components/AllProducts/AllProducts.js
@@ -8,8 +8,10 @@ const AllProducts = (props) => {
   let allProductsOutput;
   if (props.allProducts && props.allProducts.length > 0) {
     allProductsOutput = props.allProducts.map((product, i) => {
+      let stock = product.features.inStock > 0 ? "In Stock" : "Out Of Stock";
       return (
         <div key={product._id} className="product">
+          <span className="product__stock">{stock}</span>
           <Link
             to={`/product/${product._id}`}
             className="product__detail-link"
@@ -18,7 +20,7 @@ const AllProducts = (props) => {
             <img className="product__img" src={product.image} alt="img" />
             <div className="product__body">
               <h3 className="product__brand">{product.brand}</h3>
-              <p className="product__price">{product.price}</p>
+              <p className="product__price">{product.price}$</p>
             </div>
           </Link>
         </div>
