ec543ae4290d3b4614edacf81773f9d0bd35d669
Nyi Nyi <nyinyihmueaung19@gmail.com>
2021-09-24 13:57:34 +0630

    questions are fetched from database with productId

--- client/src/products/components/ProductDetail/ReviewsAndQuestions/Questions/Questions.jsx
+++ client/src/products/components/ProductDetail/ReviewsAndQuestions/Questions/Questions.jsx
@@ -1,45 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useParams } from "react-router-dom";
 
 import "./Questions.css";
-
-const questions = [
-  {
-    question: { q: "Can I get this for free?", timeStamp: "2 days ago" },
-    answer: { a: "No, you can't", timeStamp: "3 hours ago" },
-  },
-  {
-    question: {
-      q: "When the item is delivered and if it was damaged before arriving at my hands, how would you take responsibility for that?",
-      timeStamp: "2 days ago",
-    },
-    answer: {
-      a: "Of course, we try to ensure that never happens. But if it does happen, technically it will be the delivery's responsibility. But we will do anything we can to make sure that our customer doesn't have to lose in that case.",
-      timeStamp: "3 hours ago",
-    },
-  },
-  {
-    question: { q: "Can I get this for free?", timeStamp: "2 days ago" },
-    answer: { a: "No, you can't", timeStamp: "3 hours ago" },
-  },
-];
+import axios from "axios";
 
 const Questions = (props) => {
-  const qasLists = questions.map((q, i) => {
-    return (
-      <div key={i} className="qa">
-        <p className="qa__content">
-          <span className="qa__text">question</span>
-          <span>{q.question.q}</span>
-          <span className="qa__timestamp">({q.question.timeStamp})</span>
-        </p>
-        <p className="qa__content">
-          <span className="qa__text">answer</span>
-          <span>{q.answer.a}</span>
-          <span className="qa__timestamp">({q.answer.timeStamp})</span>
-        </p>
+  const { id: productId } = useParams();
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/product/${productId}/question`
+      );
+      setQuestions(resp.data);
+    })();
+  }, []);
+
+  const qasLists =
+    questions && !!questions.length ? (
+      questions.map((q, i) => {
+        return (
+          <div key={i} className="qa">
+            <p className="qa__content">
+              <span className="qa__text">question</span>
+              <span>{q.question.q}</span>
+              <span className="qa__timestamp">({q.question.timeStamp})</span>
+            </p>
+            <p className="qa__content">
+              <span className="qa__text">answer</span>
+              <span>{q.answer.a}</span>
+              <span className="qa__timestamp">({q.answer.timeStamp})</span>
+            </p>
+          </div>
+        );
+      })
+    ) : (
+      <div>
+        <p>No Questions here. Ask one.</p>
       </div>
     );
-  });
   return <div className="qas">{qasLists}</div>;
 };
 
