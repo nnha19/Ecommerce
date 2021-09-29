ad671f229c5d31728e35c2dbbac1d952587fedf4
Nyi Nyi <nyinyihmueaung19@gmail.com>
2021-09-29 16:52:17 +0630

    fixes minor issues

--- client/src/products/components/AllProducts/AllProducts.js
+++ client/src/products/components/AllProducts/AllProducts.js
@@ -1,22 +1,13 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 
 import "./AllProducts.css";
 import { useHistory } from "react-router-dom";
 
 import AddToCart from "../../components/ProductDetail/ProductDetailBody/AddToCart/AddToCart";
 import AddToWhilist from "../../../cart/components/Cart/AddToWhilist/AddToWhilist";
-import FilterProducts from "../FilterProducts/FilterProducts";
-import Pagination from "../Pagination/Pagination";
 import OverallRatings from "../OverallRatings/OverallRatings";
-import { FilterContext } from "../../../contexts/filterContext";
 
-const AllProducts = ({ allProducts, style, homePage }) => {
-  let showFilter, setShowFilter;
-  const filterContext = useContext(FilterContext);
-  if (homePage) {
-    ({ showFilter, setShowFilter } = filterContext);
-  }
-  const [resultProducts, setResultProducts] = useState(allProducts);
+const AllProducts = ({ allProducts, style }) => {
   const history = useHistory();
   let allProductsOutput;
 
@@ -29,10 +20,8 @@ const AllProducts = ({ allProducts, style, homePage }) => {
       return;
     history.push(url);
   };
-  //loop through allProducts in similarProducts page. resultProducts for homePage
-  const mapArr = homePage ? resultProducts : allProducts;
 
-  allProductsOutput = mapArr.map((product, i) => {
+  allProductsOutput = allProducts.map((product, i) => {
     let stock = product.features.inStock > 0 ? "In Stock" : "Out Of Stock";
 
     return (
@@ -66,23 +55,10 @@ const AllProducts = ({ allProducts, style, homePage }) => {
     );
   });
 
-  const returnChildren = (
+  return (
     <div style={style} className={`all-products `}>
       {allProductsOutput}
     </div>
-  );
-  //if homepage is true homepage. Otherwise similar products being used from productDetail
-  return homePage ? (
-    <>
-      {returnChildren}
-
-      {/* <Pagination
-        allProducts={allProducts}
-        setResultProducts={setResultProducts}
-      /> */}
-    </>
-  ) : (
-    returnChildren
   );
 };
 
