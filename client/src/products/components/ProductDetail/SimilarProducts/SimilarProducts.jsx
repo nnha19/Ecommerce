6a2d3002cfd4ea1146a4e54972e039883b9d473c
Nyi Nyi <nyinyihmueaung19@gmail.com>
2021-10-01 18:05:10 +0630

    skeleton loading displays for similar products

--- client/src/products/components/ProductDetail/SimilarProducts/SimilarProducts.jsx
+++ client/src/products/components/ProductDetail/SimilarProducts/SimilarProducts.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useHttp } from "../../../../customHooks/useHttp";
+import SkeletonLoading from "../../../../share/UI/SkeletonLoading/SkeletonLoading";
 
 import AllProducts from "../../AllProducts/AllProducts";
 
 const SimilarProducts = (props) => {
   const { id } = useParams();
   const [moreProducts, setMoreProducts] = useState([]);
-  const [allProducts, , , fetchData, , , ,] = useHttp();
+  const [allProducts, loading, , fetchData, , , ,] = useHttp();
   useEffect(() => {
     fetchData(`${process.env.REACT_APP_BACKEND_URL}/products`, "get");
   }, [id]);
@@ -35,9 +36,12 @@ const SimilarProducts = (props) => {
     setMoreProducts(finalResult);
   }, [allProducts]);
 
-  return moreProducts && moreProducts.length > 0 ? (
-    <AllProducts style={{ padding: "2rem 0" }} allProducts={moreProducts} />
-  ) : null;
+  return (
+    <>
+      <SkeletonLoading show={loading} />
+      <AllProducts style={{ padding: "2rem 0" }} allProducts={moreProducts} />
+    </>
+  );
 };
 
 export default SimilarProducts;
